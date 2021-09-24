714f589c872db8b9a84c2c82eee90fc7139567d0
WanyongFeng <46601448+WanyongFeng@users.noreply.github.com>
2021-09-23 21:27:24 -0400

    dfd

--- data/change_cube.py
+++ data/change_cube.py
@@ -8,28 +8,28 @@ import numpy as np
 import pickle
 
 
-dfile = 'C:/Users/wanyong/Desktop/Archive/data/cube_20_0.3_ori.pkl'
+dfile = 'cube_20_0.3.pkl'
 with open(dfile, 'rb') as f:
             data = pickle.load(f)
 train_x, train_y = data['train']
 valid_x, valid_y = data['valid']
 train_x = np.concatenate((train_x, valid_x))
-train_x = train_x.flatten()
-indices = np.random.choice(np.arange(train_x.size), replace=False, size=int(train_x.size * 0.5))
-train_x[indices] = 0
-train_x = train_x.reshape([15000, 20])
+# train_x = train_x.flatten()
+# indices = np.random.choice(np.arange(train_x.size), replace=False, size=int(train_x.size * 0.5))
+# train_x[indices] = 0
+# train_x = train_x.reshape([15000, 20])
 train_y = np.concatenate((train_y, valid_y))
 data['train'] = (train_x, train_y)
 data.pop('valid', None)
 
-test_x, test_y = data['test']
-test_x = test_x.flatten()
-indices = np.random.choice(np.arange(test_x.size), replace=False, size=int(test_x.size * 0.5))
-test_x[indices] = 0
-test_x = test_x.reshape([5000, 20])
-data['test'] = (test_x, test_y)
+# test_x, test_y = data['test']
+# test_x = test_x.flatten()
+# indices = np.random.choice(np.arange(test_x.size), replace=False, size=int(test_x.size * 0.5))
+# test_x[indices] = 0
+# test_x = test_x.reshape([5000, 20])
+# data['test'] = (test_x, test_y)
 
-dfile = 'C:/Users/wanyong/Desktop/Archive/data/cube50.pkl'
+dfile = 'cube.pkl'
 with open(dfile, 'wb') as f:
             pickle.dump(data, f)
 
